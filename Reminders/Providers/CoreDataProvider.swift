557770680cb8e940072f50fe549ca7dbdea3e5e8
wizenheimer <xNayanKumar@gmail.com>
2025-05-18 17:35:52 +0530

    feat: add core data providers

--- Reminders/Providers/CoreDataProvider.swift
+++ Reminders/Providers/CoreDataProvider.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import CoreData
+
+// Responsible for setting the core data
+class CoreDataProvider {
+    
+    // This ensures CoreDataProvider operates as a singleton
+    static let shared = CoreDataProvider()
+    
+    let persistentContainer: NSPersistentContainer
+    
+    // By making the initializer private this cannot be called from outside
+    private init() {
+        // register the custom  transformer
+        ValueTransformer.setValueTransformer(
+            UIColorTransformer(),
+            forName: NSValueTransformerName(rawValue: "UIColorTransformer")
+        )
+        
+        
+        // prepare the persistent container
+        persistentContainer = NSPersistentContainer(name: "RemindersModel")
+        persistentContainer.loadPersistentStores { description, error in
+            if let error = error as NSError? {
+                fatalError("Error initializing RemindersModel: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
