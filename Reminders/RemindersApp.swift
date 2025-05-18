862cabcf0b9f1d03f6d6f1b38bb5bc3cb7d911b6
wizenheimer <xNayanKumar@gmail.com>
2025-05-18 14:10:13 +0530

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Nayan on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
